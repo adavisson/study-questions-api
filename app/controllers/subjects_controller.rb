b679cf350c4df47b0c9825377564167051990def
Andrew Davisson <akdavisson4@gmail.com>
2020-02-20 08:47:47 -0700

    subjects routes started

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,2 +1,12 @@
 class SubjectsController < ApplicationController
+  def index
+    subjects = Subject.all
+    render json: subjects
+  end
+
+  def show
+    #format this better
+    questions = Subject.find(params[:id]).questions
+    render json: questions
+  end
 end
