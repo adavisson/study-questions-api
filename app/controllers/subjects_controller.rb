7ad1206a216bda17db6fd486cc3e61c9d5b2747a
Andrew Davisson <akdavisson4@gmail.com>
2020-02-21 09:14:25 -0700

    limited fields

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,12 +1,12 @@
 class SubjectsController < ApplicationController
   def index
     subjects = Subject.all
-    render json: subjects
+    render json: subjects, only: [:id, :name]
   end
 
   def show
     #format this better
     questions = Subject.find(params[:id]).questions
-    render json: questions
+    render json: questions, only: [:id, :question, :answer, :subject_id]
   end
 end
