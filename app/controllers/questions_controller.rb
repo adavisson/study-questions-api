bc62ef73a3670c48134c186b566c81ebf7c0a0eb
Andrew Davisson <akdavisson4@gmail.com>
2020-03-16 07:00:47 -0400

    added show route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,9 @@ class QuestionsController < ApplicationController
     questions = Question.all
     render json: questions, only: [:id, :question, :answer]
   end
+
+  def show
+    question = Question.find(params[:id])
+    render json: question, only: [:id, :question, :answer]
+  end
 end
