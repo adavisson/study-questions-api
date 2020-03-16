6e94a681ca9da4b58a328342a6986cdd001b1a4f
Andrew Davisson <akdavisson4@gmail.com>
2020-03-16 07:03:32 -0400

    added create route

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,9 @@ class QuestionsController < ApplicationController
     question = Question.find(params[:id])
     render json: question, only: [:id, :question, :answer]
   end
+
+  def create
+    question = Question.find_or_create_by(question: params[:question], answer: params[:answer], subject_id: params[:subject_id])
+    render json: question
+  end
 end
